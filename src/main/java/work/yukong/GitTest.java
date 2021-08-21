227bf85eb04b12829aca3ed54743449312520fb2
xiaoyu <1499339872@qq.com>
2021-08-21 18:07:50 +0800

    test 1

--- src/main/java/work/yukong/GitTest.java
+++ src/main/java/work/yukong/GitTest.java
@@ -8,5 +8,10 @@ package work.yukong;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git2");
+        System.out.println("hello git3");
+        System.out.println("hello git4");
+        System.out.println("hello git5");
+
     }
 }
