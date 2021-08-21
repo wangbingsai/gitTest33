d0ae374c103e963b00db0f652a556c021f212c5c
xiaoyu <1499339872@qq.com>
2021-08-21 19:05:46 +0800

    master commit3

--- src/main/java/work/yukong/GitTest.java
+++ src/main/java/work/yukong/GitTest.java
@@ -14,5 +14,6 @@ public class GitTest {
         System.out.println("hello git4");
         System.out.println("hello git5");
         System.out.println(123);
+        System.out.println(6666666);
     }
 }
